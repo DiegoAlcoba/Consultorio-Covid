1dd3effdff8ac70c70b4a1f8c29e5c95871d0783
DiegoAlcoba <diegoalcobaarias@gmail.com>
2021-11-29 20:46:29 +0100

    Meh

--- consultorio.c
+++ consultorio.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -8,11 +9,18 @@
 
 int main(int argc, char *argv[]) {
 
-	//El script pasa el argumento correctamente y lo imprime como un array de chars
-	printf("Se está tensando, por lo que el número de pacientes es %s\n", argv[1]);
+	int parameter; 
+	parameter = atoi(argv[1]); //Paso el argumento pasado a int
 
-	//Ahora hacer que el parámetro pase a ser un int
-	int parameter;
+	pid_t pid;
+	int *hijos;
+	hijos = (int *) malloc ((3 * sizeof (int));
+
+	for (int i = 0; i <= hijos; i++) {
+		
+		pid = fork();
+	}
+
 
 return 0;
 }
