b1dff13540a604708b47db53271be65399a501b7
DiegoAlcoba <diegoalcobaarias@gmail.com>
2021-12-01 14:27:02 +0100

    Consultorio algo avanzado

--- consultorio.c
+++ consultorio.c
@@ -7,21 +7,62 @@
 #include <sys/wait.h> 
 #include <sys/syscall.h>
 
+void preparaVacunas(pid_t c, pid_t r,int v) {
+	
+	printf("El número de vacunas disponibles son %d\n", v);	
+
+}
+
 int main(int argc, char *argv[]) {
+	
+	srand(getpid());
 
-	int parameter; 
-	parameter = atoi(argv[1]); //Paso el argumento pasado a int
+	int parameter, vacunas, estado;
+	int *pacientes; //Recordar liberar la memoria al final "free(pacientes)"
+	pid_t pidCoord, pidResp, pidAux1, pidAux2;
 
-	pid_t pid;
-	int *hijos;
-	hijos = (int *) malloc ((3 * sizeof (int));
+	// Paso el argumento del programa (pacientes) como entero y creo un array dinámico del tamaño del número de pacientes 
+	parameter = atoi(argv[1]);
+	pacientes = (int *) malloc (parameter * sizeof (int));
 
-	for (int i = 0; i <= hijos; i++) {
+	vacunas = rand(); //Número vacunas disponibles aleatoria
+
+	//Creación del responsable de inventario y los dos auxiliares
+	//int *hijos;
+	//hijos = (int *) malloc (3 * sizeof (int));
+	pidCoord = wait(&estado);
+
+	for (int i = 0; i <= 2; i++) {
+		pidCoord = fork();
 		
-		pid = fork();
+		if (pidCoord == -1) {
+			perror("Error en la llamada a fork()");
+		} else if (pidCoord == 0) {
+			switch (i) {
+				case 0:
+				//printf("Soy el responsable de inventario %d\n", getpid());
+					pidResp = getpid();
+					sleep(3);
+					break;
+				case 1:
+					//printf("Soy el aux1 %d\n", getpid());
+					pidAux1 = getpid();
+					sleep(3);
+					break;
+				case 2:
+					//printf("Soy el aux2 %d\n", getpid());
+					pidAux2 = getpid();
+					sleep(3);
+					break;
+			}
+		} else {
+			wait(&estado);
+			//printf("Soy el coordinador %d\n", getpid());
+		}
 	}
 
+	//Llamo a la función para que el coordinador prepare las vacunas
+	preparaVacunas(pidCoord, pidResp, vacunas);
 
 return 0;
 }
-
