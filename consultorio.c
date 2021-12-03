55a0ea577d754d4205c1b7f2e9a4d7d47c4718cd
DiegoAlcoba <diegoalcobaarias@gmail.com>
2021-12-03 01:29:36 +0100

    A puntito

--- consultorio.c
+++ consultorio.c
@@ -7,7 +7,7 @@
 #include <sys/wait.h> 
 #include <sys/syscall.h>
 
-/*Función que calcula aleatoriamente si hay vacunas suficientes o no*/
+/*Función que calcula números aleatorios*/
 int calculaAleatorios(int min, int max) {
 	srand(getpid());
 
@@ -17,27 +17,33 @@ int calculaAleatorios(int min, int max) {
 /*Preparación de las vacunas*/
 void handle_preparaVacunas(int sig) {
 
-	/*Se vuelve a asignar el handler a la señal SIGUSR1*/
+	/*Se vuelve a asignar el handler a la señal SIGUSR0*/
 	if (signal(SIGUSR1, handle_preparaVacunas) == SIG_ERR) {
 		perror("Llamada a signal");
 		exit(-1);
 	}
 
-	return calculaAleatorios(0, 1);
+	printf("** Responsable de inventario preparando las vacunas **\n");
+	sleep(calculaAleatorios(3, 6));
+	
+	exit(calculaAleatorios(0, 1));
+
 }
 
 /*Auxiliares reciben la orden de proceder con la vacunación*/
 void handle_vacunacion(int sig) {
 
-	printf("** Procediendo a la vacunación **");
+	printf("** Procediendo a la vacunación **\n");
+
 }
 
 int main(int argc, char *argv[]) {
 
-	int parameter, estado, vacunas;
+	int parameter, estado, vacunas, i;
 	int *pacientes; //Recordar liberar la memoria al final "free(pacientes)"
 
 	/* Paso el argumento del programa (pacientes) como entero y creo un array dinámico del tamaño del número de pacientes*/ 
+
 	parameter = atoi(argv[1]);
 	pacientes = (int *) malloc (parameter * sizeof (int));
 
@@ -48,19 +54,20 @@ int main(int argc, char *argv[]) {
 	/*Creación del responsable de inventario y los dos auxiliares*/
 	pid_t p;
 
-	for (int i = 0; i < 3; i++) {
-		
-		p = fork();
-		hijos[i] = p;
-		
+	for (i = 0; i < 3; i++) {
+
+	    p = fork();
+
 		if (p == -1) {
+
 			perror("Error en la llamada a fork()\n");
+
+			exit(-1);
 		} 
 		else if (p == 0) {
+
 			switch (i) {
 				case 0: /*Responsable del inventario*/
-					
-					signal(SIGUSR1, &handle_preparaVacunas); /*Recibe la señal*/
 					
 					/*Se le asignal el handler a la señal SIGUSR1*/
 					if (signal(SIGUSR1, handle_preparaVacunas) == SIG_ERR) { 
@@ -69,45 +76,59 @@ int main(int argc, char *argv[]) {
 						exit(-1);
 					}
 
-					printf("** Responsable de inventario preparando las vacunas **\n");
-					sleep(calculaAleatorios(3, 6));
-					
-					exit(handle_preparaVacunas(vacunas));
+					pause();
+
 					break;
 				case 1: /*Auxiliar 1*/
 				case 2: /*Auxiliar 2*/
 
-					signal(SIGUSR2, &handle_vacunacion);	
+					// signal(SIGUSR2, &handle_vacunacion);	
+					// signal(SIGUSR2, handle_vacunacion);	
+					/*Se le asignal el handler a la señal SIGUSR1*/
+					if (signal(SIGUSR2, handle_vacunacion) == SIG_ERR) { 
+						perror("Llamada a signal");
 					
+						exit(-1);
+					}
+
+					pause();
+					exit(0);
+
+					// vacunar
+
 					break;
 			}
 		} 
 		else { /*Coordinador*/
 
-			kill(hijos[0], SIGUSR1);
-			wait(&estado);
-
-			if (WEXITSTATUS == 0) { /*No hay vacunas suficientes*/
-				for (int i = 0; i < 3; i++) {
-					kill(hijos[i], SIGTERM);
-				}
-
-				exit(-1);
-			}
-			else if (WEXITSTATUS == 1) { /*Hay vacunas suficientes*/
-				
-				for (int i = 1; i < 3; i++) {
-					kill(hijos[i], SIGUSR2);
-				}
-				
-
-			}
-			else {
-				perror("** Error en el número de vacunas **\n");
-			}
-
+            hijos[i] = p;
 		}
 	}
 
-return 0;
+	kill(hijos[0], SIGUSR1);
+	wait(&estado);
+
+	int ret = WEXITSTATUS(estado);
+
+	if (ret == 0) { /*No hay vacunas suficientes*/
+		// for (int i = 1; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
+			kill(hijos[i], SIGTERM);
+		}
+
+		// printf no hay vacunas
+		exit(0);
+	}
+	else if (ret == 1) { /*Hay vacunas suficientes*/
+		for (int i = 1; i < 3; i++) {
+			kill(hijos[i], SIGUSR2);
+		}
+	}
+	else {
+		perror("** Error en el número de vacunas **\n");
+	}
+
+	free(hijos);
+	
+	return 0;
 }
